1dff11f10dc956d7aa283c017af61b2c6a9ad2f7
abhinav-m22 <abhinavpm05@gmail.com>
2023-05-31 23:09:08 +0530

    Added Navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,5 +2,15 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <head>
+        <title>Sleetcode</title>
+        <meta name='viewport' content='width-device-width, initial-scale-1' />
+        <link rel="icon" href="/favicon.png" />
+        <meta name='description' content='Web Application that contains Leetcode problems and Code editor ' />
+      </head>
+      <Component {...pageProps} />
+    </>
+  )
 }
